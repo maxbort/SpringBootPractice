8d072ae8047f62e67b291ddf294d4105a837f8b0
maxbort <cjfgh2449@naver.com>
2022-03-08 22:20:46 +0900

    구글 연동 로그인 구현

---
+++ src/main/java/com/example/SpringBootPractice/config/auth/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.SpringBootPractice.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
+
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomUserTypesOAuth2UserService customUserTypesOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable().headers().frameOptions().disable()
+                .and().authorizeRequests().antMatchers("/","/css/**","/images/**"
+                ,"/js/**", "/h2-console/**").permitAll().antMatchers("/api/v1/**")
+                .hasRole(Role.User.name()).anyRequest().authenticated().and().logout()
+                .logoutSuccessUrl("/").and().oauth2Login().userInfoEndpoint()
+                .userService(customUserTypesOAuth2UserService);
+    }
+
+}
